24c494897d02880c43be004ad79585cbf5f1f661
Michael Curry <michaelcurry95@gmail.com>
2021-11-25 12:45:54 -0500

    drying up views

--- views/contact.view.php
+++ views/contact.view.php
@@ -1,15 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php include('partials/head.php') ?>
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Contact</title>
-</head>
 
-<body>
-  <h1>Contact Us</h1>
-</body>
-
-</html>
+<h1>Contact Us</h1>
+<?php include('partials/footer.php') ?>
--- views/index.view.php
+++ views/index.view.php
@@ -1,35 +1,17 @@
-<?php
-
-use function PHPSTORM_META\map;
-?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Rules!</title>
-    <link rel="stylesheet" href="css/index.css" />
-</head>
-
-<body>
-    <h1>Tasks</h1>
-    <ul>
-        <?php foreach ($tasks as $task) : ?>
-            <?php if ($task->completed) : ?>
-                <strike>
-                    <li><?= $task->description ?></li>
-                </strike>
-            <?php else : ?>
-
+<?php include('partials/head.php') ?>
+<h1>Tasks</h1>
+<ul>
+    <?php foreach ($tasks as $task) : ?>
+        <?php if ($task->completed) : ?>
+            <strike>
                 <li><?= $task->description ?></li>
+            </strike>
+        <?php else : ?>
 
-            <?php endif ?>
+            <li><?= $task->description ?></li>
 
-        <?php endforeach ?>
-    </ul>
-    <script src="js/index.js"></script>
-</body>
+        <?php endif ?>
 
-</html>
+    <?php endforeach ?>
+</ul>
+<?php include('partials/footer.php') ?>
